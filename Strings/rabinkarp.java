ae865bc63cb6a6480e50d28c1be1a482149ec128
Nithin R <37147336+thebinarybot@users.noreply.github.com>
2024-11-13 12:38:02 +0000

    MOD: Corner Case Fixed

--- Strings/rabinkarp.java
+++ Strings/rabinkarp.java
@@ -2,7 +2,7 @@ public class rabinkarp {
     public static void main(String[] args) {
         String text = "interviewprepPREP";
         String pattern = "PREP";
-        System.out.println("Substring Found At Index:" + RabinKarp(text, pattern));
+        System.out.println("Substring Found At Index: " + RabinKarp(text, pattern));
     }
 
     public static int RabinKarp(String text, String pattern) {
@@ -15,33 +15,32 @@ public class rabinkarp {
         int currentWindowHash = 0;
 
         // Preprocessing
-        for (int i = 0; i < patternLength - 1; i++) {
+        for (int i = 0; i < patternLength; i++) {
             patternHash = (alphabetSize * patternHash + pattern.charAt(i)) % primeModulous;
             currentWindowHash = (alphabetSize * currentWindowHash + text.charAt(i)) % primeModulous;
         }
 
         // Slide over the text
-        for(int i=0;i<textLength-patternLength;i++){
-            if(patternHash==currentWindowHash){
-                boolean isMatch=true;
+        for (int i = 0; i <= textLength - patternLength; i++) { 
+            if (patternHash == currentWindowHash) {
+                boolean isMatch = true;
                 // Verification to minimize collision
-                for(int j=0;j<patternLength-1;j++){
-                    if(text.charAt(i+j)!=pattern.charAt(j)){
-                        isMatch=false;
+                for (int j = 0; j < patternLength; j++) { 
+                    if (text.charAt(i + j) != pattern.charAt(j)) {
+                        isMatch = false;
                         break;
                     }
                 }
-                if(isMatch){
+                if (isMatch) {
                     return i;
                 }
             }
 
-            // Check if we are the last window, then
-            // Slide window by removing the leftmost and adding the immediete right
-            if(i<textLength-patternLength){
-                currentWindowHash=(alphabetSize*(currentWindowHash-text.charAt(i)*hashFactor)+text.charAt(i+patternLength))%primeModulous;
-                if(currentWindowHash<0){
-                    currentWindowHash+=primeModulous;
+            // Slide window by removing the leftmost and adding the immediate right
+            if (i < textLength - patternLength) {
+                currentWindowHash = (alphabetSize * (currentWindowHash - text.charAt(i) * hashFactor) + text.charAt(i + patternLength)) % primeModulous;
+                if (currentWindowHash < 0) {
+                    currentWindowHash += primeModulous;
                 }
             }
         }
