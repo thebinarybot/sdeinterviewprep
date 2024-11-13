e44698bce0cb76c525387eb7c4af691dd4d32c29
Nithin R <37147336+thebinarybot@users.noreply.github.com>
2024-11-13 12:23:21 +0000

    ADD: Rabin Karp Java Code

---
+++ Strings/rabinkarp.java
@@ -0,0 +1,52 @@
+public class rabinkarp {
+    public static void main(String[] args) {
+        String text = "interviewprepPREP";
+        String pattern = "PREP";
+        System.out.println("Substring Found At Index:" + RabinKarp(text, pattern));
+    }
+
+    public static int RabinKarp(String text, String pattern) {
+        int primeModulous = 101;
+        int patternLength = pattern.length();
+        int textLength = text.length();
+        int alphabetSize = 256;
+        int hashFactor = (int) Math.pow(alphabetSize, patternLength - 1) % primeModulous;
+        int patternHash = 0;
+        int currentWindowHash = 0;
+
+        // Preprocessing
+        for (int i = 0; i < patternLength - 1; i++) {
+            patternHash = (alphabetSize * patternHash + pattern.charAt(i)) % primeModulous;
+            currentWindowHash = (alphabetSize * currentWindowHash + text.charAt(i)) % primeModulous;
+        }
+
+        // Slide over the text
+        for(int i=0;i<textLength-patternLength;i++){
+            if(patternHash==currentWindowHash){
+                boolean isMatch=true;
+                // Verification to minimize collision
+                for(int j=0;j<patternLength-1;j++){
+                    if(text.charAt(i+j)!=pattern.charAt(j)){
+                        isMatch=false;
+                        break;
+                    }
+                }
+                if(isMatch){
+                    return i;
+                }
+            }
+
+            // Check if we are the last window, then
+            // Slide window by removing the leftmost and adding the immediete right
+            if(i<textLength-patternLength){
+                currentWindowHash=(alphabetSize*(currentWindowHash-text.charAt(i)*hashFactor)+text.charAt(i+patternLength))%primeModulous;
+                if(currentWindowHash<0){
+                    currentWindowHash+=primeModulous;
+                }
+            }
+        }
+
+        // If there is no match, return -1
+        return -1;
+    }
+}
